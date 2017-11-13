563721cc134c1a4dfe088180bb39daac5a5203f5
Anmol Vijayvargiya <anmol.vijay94@gmail.com>
2017-11-12 23:02:28 -0800

    implemented email validation regex

--- Main.rb
+++ Main.rb
@@ -135,6 +135,7 @@ put '/student/:id' do
 		session[:student_error_message] = "Birthday Day field is Mandetory"
 		redirect '/student/'+params[:id]
 	end
+
 	if (session[:student_error_message] == "")
 		@student.update(firstname: params[:firstname], lastname: params[:lastname], address: params[:address], student_id: params[:student_id], extrainfo: params[:extrainfo], birthday_year: params[:birthday_year], birthday_month: params[:birthday_month], birthday_day: params[:birthday_day])
 		redirect '/student/'+params[:id]
@@ -183,24 +184,55 @@ end
 
 # get signup form
 get '/signup' do
+	@signup_error_message = session[:signup_error_message]
+	session[:signup_error_message] = ""
 	erb :signup
 end
 
 # validate signup form
 post '/signup' do
-	@user = User.new
-	@user.name = params[:name]
-	@user.email = params[:email]
-	@user.password = params[:password]
-	if (User.count(email: @user.email, password: @user.password) == 0)
-		@user.save
-		session[:user_id] = @user.id
-		session[:error_message] = ""
-		redirect '/logout'
-	else
-		session[:error_message] = "The user with given credentials already exists"
-		redirect '/login'
+	session[:signup_error_message] = ""
+	if (params[:name].strip.empty?)
+		session[:signup_error_message] = "Name field cannot be blank"
+		redirect '/signup'
 	end
+	if (params[:email].strip.empty?)
+		session[:signup_error_message] = "Email field cannot be black"
+		redirect '/signup'
+	end
+	if (params[:password].strip.empty?)
+		session[:signup_error_message] = "Please enter a password"
+		redirect '/signup'
+	end
+
+	# email validation using regex expression
+	if (params[:email] =~ /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+  	@email_is_valid = true
+	else
+	  @email_is_valid = false
+	end
+
+	if ( !@email_is_valid )
+		session[:signup_error_message] = "The email entered is invalid"
+		redirect '/signup'
+	end
+
+	if (session[:signup_error_message] == "")
+			@user = User.new
+			@user.name = params[:name]
+			@user.email = params[:email]
+			@user.password = params[:password]
+			if (User.count(email: @user.email, password: @user.password) == 0)
+				@user.save
+				session[:user_id] = @user.id
+				session[:error_message] = ""
+				redirect '/logout'
+			else
+				session[:error_message] = "The user with given credentials already exists"
+				redirect '/login'
+			end
+	end
+	redirect '/login'
 end
 
 #login
